a65ab9811140357ec2aadb2628b26a942978db77
Ricardo Dias <ricardodias.dev@gmail.com>
2023-07-27 00:05:32 -0300

    Refatorando navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,8 @@
-"use client";
-
 import { UserButton, auth } from "@clerk/nextjs";
-import { MainNav } from "@/components/main-nav";
-import StoreSwitcher from "./store-switcher";
 import { redirect } from "next/navigation";
+
+import StoreSwitcher from "@/components/store-switcher";
+import { MainNav } from "@/components/main-nav";
 import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
@@ -22,9 +21,7 @@ const Navbar = async () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>
-          <StoreSwitcher items={stores}/>
-        </div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
