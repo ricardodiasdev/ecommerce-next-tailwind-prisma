dd188b023afabb7c65adc2877ec67b23e2015a40
Ricardo Dias <ricardodias.dev@gmail.com>
2023-08-01 23:52:07 -0300

    Desenvolvendo o componente api-alert

---
+++ components/ui/api-alert.tsx
@@ -0,0 +1,31 @@
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { Server } from "lucide-react";
+
+interface ApiAlertProps {
+  title: string;
+  description: string;
+  variant: "public" | "admin";
+}
+
+const textMap: Record<ApiAlertProps["variant"], string> = {
+  public: "Public",
+  admin: "Admin",
+};
+
+const variantMap: Record<ApiAlertProps["variant"], string> = {
+  public: "secondary",
+  admin: "destructive",
+};
+
+export const ApiAlert: React.FC<ApiAlertProps> = ({
+    title, description, variant="public"
+}) => {
+    return (
+        <Alert>
+            <Server className="h-4 w-4"/>
+            <AlertTitle className="flex items-center gap-x-2">
+                {title}
+            </AlertTitle>
+        </Alert>
+    )
+}
